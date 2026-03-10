a608e6bbf40c0966b5bcf3c2e34ac2ca36edd444
Diego Bessoudo <diego@MacBookAir.Home>
2026-03-10 13:17:13 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import QrCode from "./pages/QrCode";
 import NotFound from "./pages/NotFound";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
